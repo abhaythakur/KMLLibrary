da5de21e634b381b1ddbcc2ff01a25bdbb4aea71
abhay <abhaysingh@abhays-macbook-pro.local>
2019-04-27 19:50:14 +0530

    Initial Commit

---
+++ KMLLibrary/KMLLibrary.h
@@ -0,0 +1,19 @@
+//
+//  KMLLibrary.h
+//  KMLLibrary
+//
+//  Created by Abhay Singh on 27/04/19.
+//  Copyright © 2019 Abhay Singh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KMLLibrary.
+FOUNDATION_EXPORT double KMLLibraryVersionNumber;
+
+//! Project version string for KMLLibrary.
+FOUNDATION_EXPORT const unsigned char KMLLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KMLLibrary/PublicHeader.h>
+
+
